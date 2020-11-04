f056581c6838dae4fd06d1ec832a53adb24aef25
Josue Carlos Zenteno Yave <61275784+Josue-Zenteno@users.noreply.github.com>
2020-11-04 18:55:01 +0100

    Implemented first version of Node.py and ProblemExitMaze.py

---
+++ src/Node.py
@@ -0,0 +1,21 @@
+class Node:
+    ###########################---Attributes---############################
+    id = tuple()
+    cost = float()
+    id_state = tuple()
+    id_parent = tuple()
+    action = str()
+    depth = int()
+    heuristic = int()
+    value = float()
+
+    ###########################---Constructor---###########################
+    def __init__(self, identifier, cost, id_state, id_parent, action, depth, heuristic, value):
+        self.id = identifier
+        self.cost = cost
+        self.id_state = id_state
+        self.id_parent = id_parent
+        self.action = action
+        self.depth = depth
+        self.heuristic = heuristic
+        self.value = value
---
+++ src/ProblemExitMaze.py
@@ -0,0 +1,22 @@
+class ProblemExitMaze:
+    ###########################---Attributes---############################
+    ###########################---Constructor---###########################
+    def __init__(self):
+        pass
+
+    ###########################---Main Methods---##########################
+    @staticmethod
+    def problem_exit_maze():
+        pass
+
+    @staticmethod
+    def get_initial_state():
+        pass
+
+    @staticmethod
+    def successor_function():
+        pass
+
+    @staticmethod
+    def goal_test(problem, state):
+        pass
